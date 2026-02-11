839670c44fc82411b651bc183db141fd7cf3fa56
Armagdy <armagdy.salah@gmail.com>
2026-02-11 11:26:17 +0200

    fix to work with dev

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/mushafy/",
+  base: mode === "production" ? "/mushafy/" : "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
